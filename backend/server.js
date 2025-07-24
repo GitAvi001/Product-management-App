63e2288a7b70f9c359f574d0bb7a1101a38eda2a
GitAvi001 <avindusridharmawardhana@gmail.com>
2025-07-24 11:50:03 +0530

    backend server configurations added for requests

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,36 @@
 import express from "express" 
+import helmet from "helmet"
+import morgan from "morgan"
+import cors from "cors"
+import dotenv from "dotenv" 
+
+dotenv.config() // Load environment variables from .env file
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3000; // Use PORT from environment variables or default to 3000
+
+console.log(PORT)
+
+app.use(express.json()) // to parse JSON bodies in requests
+app.use(cors()) // to enable CORS for all routes
+//helmet use to check the security of the application with setting multiple headers
+app.use(helmet())
+
+//morgan helps to check logs of the requests 
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+    console.log(res.getHeaders())
+    res.send("Hello! This message from backend")
+})
+
+app.get("/route1", (req, res) => {
+    console.log(res.getHeaders())
+    res.send("Hello! This message from route named route1")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port "+PORT);
 });
 
