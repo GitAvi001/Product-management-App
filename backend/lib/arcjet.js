f4cad01e4fb5e393a3fc31f0febe2bf54c59285e
GitAvi001 <avindusridharmawardhana@gmail.com>
2025-08-02 12:32:16 +0530

    Initialized bot detection and rate limiting configuration using arcjet

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,33 @@
+import arcjet, {tokenBucket, shield, detectBot} from "@arcjet/node";
+
+import "dotenv/config";
+
+//initialize arcject for Rate limiting and Bot Detection
+
+export const aj=arcject({
+    key: process.env.ARCJET_KEY,
+    characteristics: ["ip.src"],
+    rules: [
+        //shield will protect against bots and malicious traffic like SQL injection, XSS, CSRF
+        shield({mode:"LIVE"}),
+        detectBot({
+            mode: "LIVE",
+            //blocking all bots except search engine bots
+            allow:
+                "CATEGORY:SEARCH_ENGINE"
+                //see full list of bots from https://arcjet.com/bot-list 
+
+        
+        }),
+
+        //Token bucket method used for rate limiting
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 5, //Refilling 5 tokens per second
+            interval: 10, //Allows 5 tokens per second
+            capacity: 10, //Allows client to burst up to 10 requests
+        }),
+
+
+    ]
+})
